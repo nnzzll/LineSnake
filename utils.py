82bd6e25ec39d9063a9511ed1ca2996fa4653947
nnzzll <nnzzll@zju.edu.cn>
2021-04-25 11:28:15 +0800

    修复极坐标转换的方向

--- utils.py
+++ utils.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 def polarToCart(r:list,theta:list,center:tuple):
     x = [center[0]+r[i]*np.cos(theta[i]) for i in range(len(r))]
-    y = [center[1]+r[i]*np.sin(theta[i]) for i in range(len(r))]
+    y = [center[1]-r[i]*np.sin(theta[i]) for i in range(len(r))]
     return x,y
 
 def img2polar(image: np.ndarray, size: tuple, center: tuple, radius: int) -> np.ndarray:
-    '''直角坐标系下的图像转换到极坐标系下,方向为逆时针方向,x轴正方向为0°
+    '''直角坐标系下的图像转换到极坐标系下,方向为顺时针方向,x轴正方向为0°
     '''
     flags = cv2.INTER_CUBIC + cv2.WARP_FILL_OUTLIERS + cv2.WARP_POLAR_LINEAR
     linear_polar_image = cv2.warpPolar(image, size, center, radius, flags)
